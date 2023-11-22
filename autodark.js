d2588a5019bb51f60cee59cb66231945ef58cd1e
alp3rkum <142728187+alp3rkum@users.noreply.github.com>
2023-11-22 14:36:25 +0300

    Create autodark.js
    Added AutoDark.JS Version 1.0

---
+++ autodark.js
@@ -0,0 +1,24 @@
+/*Time-Based Dark Mode Automation for websites
+    This basic JavaScript module will automate the process of switching the web page's theme into dark mode according to the hour of the day as recognized by the system.
+    Coded by Alper KUM, 2023
+*/
+
+const html = document.querySelector("html"); //mandatory, gets the HTML element of the page
+
+function isNightTime() { //to ensure the time is suitable for the dark mode
+    var now = new Date();
+    var hour = now.getHours();
+    return hour >= 20 || hour < 8; //doesn't really need to be changed
+}
+
+function toggleDarkMode(){
+    if (isNightTime()) {
+        //code to change the theme into dark mode, depends on the CSS layout
+    } else {
+        //code to revert the standard theme, depends on the CSS layout
+    }
+}
+
+/* window.onload = function() { //template function to check which hour it is. This is how do you launch the function.
+     setInterval(toggleDarkMode, 1000); //can be used in another JavaScript file depending on web page layout and framework.
+} */
