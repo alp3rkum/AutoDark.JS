a212791c3aa879b3402de942b75528629e7bbfd2
Alper Kum <142728187+alp3rkum@users.noreply.github.com>
2024-03-01 15:16:49 +0300

    Fix

--- js/autodark.js
+++ js/autodark.js
@@ -4,28 +4,26 @@
 */
 const html = document.querySelector("html"); //goes straight to the HTML element
 
-function isDayTime() {
+function isNightTime() {
     const date = new Date();
     const hour = date.getHours(); //gets the current hour of the day
     const season = Math.floor(((date.getMonth() + 1)%12)/4); //gets the season of the year
-
     if(season == 0) //if it's winter
-        return hour >= 7 && hour < 19;
+        return !(hour >= 7 && hour < 19);
     else if(season == 1 || season == 3) //if it's spring or autumn/fall
-        return hour >= 7 && hour < 20;
+        return !(hour >= 7 && hour < 20);
     else if (season == 2) //if it's summer
-        return hour >= 6 && hour < 21;
+        return !(hour >= 6 && hour < 21);
     else //exception handling
-        return hour >= 7 && hour < 19;
+        return !(hour >= 7 && hour < 19);
 }
 
 function run()
 {
-    console.log(isDayTime());
-    isDayTime() ? html.dataset.bsTheme = "light" : html.dataset.bsTheme = "dark"; //if it's night time, change the page's theme to dark, else change it to light
+    isNightTime() ? html.dataset.bsTheme = "dark" : html.dataset.bsTheme = "light"; //if it's night time, change the page's theme to dark, else change it to light
 }
 
 window.onload = function() {
     run();
-    setInterval(run,60000); //runs every minute
+    setInterval(run,60000); //runs once every minute
 }
